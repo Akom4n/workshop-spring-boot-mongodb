9596dce51371ed72fbe85d92ada9e5f2027e8ae1
Vinicius Silva <xdenik13@gmail.com>
2023-09-15 18:25:39 -0300

    Obtendo um usuario por id

--- src/main/java/com/akom4n/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/akom4n/workshopmongo/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.akom4n.workshopmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.akom4n.workshopmongo.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
 }
