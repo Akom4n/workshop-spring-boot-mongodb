4e18eee84a606c387ad6cf68dff02cc8030fdb53
Vinicius Silva <xdenik13@gmail.com>
2023-09-22 16:14:10 -0300

    Consulta com varios criterios

--- src/main/java/com/akom4n/workshopmongo/services/PostService.java
+++ src/main/java/com/akom4n/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.akom4n.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.akom4n.workshopmongo.domain.Post;
-import com.akom4n.workshopmongo.domain.User;
 import com.akom4n.workshopmongo.exception.ObjectNotFoundException;
 import com.akom4n.workshopmongo.repository.PostRepository;
 
@@ -28,5 +28,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
